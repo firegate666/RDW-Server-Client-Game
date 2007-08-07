fc9f3f87d4a12bb76675c828740aa7f07a20eecc
root <root>
2007-08-07 20:42:35 +0000

    custom cell renderer for transparency

--- src/de/mb/rdw/swing/TransparentComboBox.java
+++ src/de/mb/rdw/swing/TransparentComboBox.java
@@ -1,10 +1,12 @@
 package de.mb.rdw.swing;
 
+import java.awt.Component;
+
 import javax.swing.JComboBox;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
 
 import org.apache.log4j.Logger;
-
-import de.mb.util.Utils;
 
 public class TransparentComboBox extends JComboBox {
 	final static Logger log = Logger.getLogger(TransparentComboBox.class);
@@ -20,6 +22,17 @@ public class TransparentComboBox extends JComboBox {
 
 	protected void initialize() {
 		setOpaque(false);
-	}
+		setRenderer(new ListCellRenderer() {
 
+			public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+				TransparentLabel next = new TransparentLabel();
+				if (value != null)
+					next.setText(value.toString());
+				return next;
+			}
+
+		});
+	}
 }
+
+
